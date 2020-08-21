4e8d9a43be136b0382721dcb35df635dda9bdb01
ryousuke-515 <ryo_suke515@icloud.com>
2020-08-21 14:54:07 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update,]
+
+  resources :rooms, only: [:new, :create]
 end
